d93b64d5410da7d2b6f5350103df27a3b3c50ca5
lihxhit <lihx_hit@163.com>
2018-07-16 14:34:31 +0800

    整理下代码

--- index.js
+++ index.js
@@ -5,41 +5,12 @@ const util = require('./lib/util');
 const validate = require('./lib/validate');
 module.exports = cas;
 
-const DEFAULT_OPTIONS = {
-    servicePrefix: '',
-    serverPath:'',
-    session: {
-        key: 'cas'
-    },
-    paths: {
-        homePage: '/',
-        login: '/sso/login/',
-        logout: {
-            cas: '/sso/logout',
-            server: ''
-        },
-        serviceValidate: '/sso/serviceValidate',
-    },
-    ajax:{
-        header:{
-            key:'nox-ajax',
-            value:'1'
-        },
-        response:{
-            errNum:10010,
-            message:"no login",
-            data:undefined,
-        }
-    },
-    casInfoFormat(info){
-        return info;
-    }
-};
+const DEFAULT_OPTIONS = require('./lib/config');
 function cas(opts, next) {
     opts = _.merge(DEFAULT_OPTIONS, opts);
     opts.ajax.response.data = opts.ajax.response.data || {
-        origin:opts.serverPath,
-        login:opts.serverPath
+        origin: opts.serverPath,
+        login: opts.serverPath
     }
     return async function cas(ctx, next) {
         if (!ctx.session) {
@@ -48,46 +19,56 @@ function cas(opts, next) {
         if (!opts.servicePrefix) {
             throw new Error('opts.servicePrefix required');
         }
-        // if (!opts.servicePrefix) {
-        opts.service = ctx.href;
-        // }
-        const context = {
-            ctx,
-            opts
-        }
-        const path = ctx.path;
-        const method = ctx.method;
-        if (opts.paths.logout.server) {
-            if (util.pathEqual(path, opts.paths.logout.server)) {
-                ctx.session = null;
-                debug("match logout");
-                return ctx.redirect(util.getPath('logout', context));
+        try {
+            opts.service = ctx.href;
+            const context = {
+                ctx,
+                opts
             }
-        }
+            const path = ctx.path;
+            if (opts.paths.logout.server) {
+                if (util.pathEqual(path, opts.paths.logout.server)) {
+                    ctx.session = null;
+                    debug("match logout");
+                    return ctx.redirect(util.getPath('logout', context));
+                }
+            }
 
-        if (util.shouldIgnore(path, opts)) {
-            debug("match ignore || session");
+            if (util.shouldIgnore(path, opts)) {
+                debug("match ignore || session");
+                if (ctx.query.ticket) {
+                    debug("redirect noticket");
+                    return ctx.redirect(util.getPath('noticket', context));
+                }
+
+            }
             if (ctx.query.ticket) {
-                debug("redirect noticket");
+                const response = await validate(ctx.query.ticket, context);
+                const sessionKey = opts.session.key;
+                if (response) {
+                    if (opts.casInfoFormat) {
+                        ctx.session[sessionKey] = opts.casInfoFormat(response);
+                    } else {
+                        ctx.session[sessionKey] = response;
+                    }
+                }e
+                debug("redirect noticket after validate");
                 return ctx.redirect(util.getPath('noticket', context));
             }
-            
+            if (ctx.session[opts.session.key]) {
+                return await next();
+            }
+            if (!util.isAjax(context)) {
+                ctx.redirect(util.getPath('login', context));
+            } else {
+                ctx.status = 403;
+                ctx.body = opts.ajax.response;
+                return;
+            }
+            await next();
+        } catch (error) {
+            throw error; 
         }
-        if (ctx.query.ticket) {
-            await validate(ctx.query.ticket, context);
-            debug("redirect noticket after validate");
-            return ctx.redirect(util.getPath('noticket', context));
-        }
-        if(ctx.session[opts.session.key]){
-            return await next();
-        }
-        if(!util.isAjax(context)){
-            ctx.redirect(util.getPath('login', context));
-        }else{
-            ctx.status = 401;
-            ctx.body = opts.ajax.response;
-            return;
-        }
-        await next();
+
     }
 }
